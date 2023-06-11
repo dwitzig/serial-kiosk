2480e800296df51c0c81f9e9432261464eda6d3b
Damien <damien@webts.biz>
2023-06-11 11:14:25 +1000

    test device_id in wrapper

--- electron/preload.js
+++ electron/preload.js
@@ -2,7 +2,8 @@
 const { contextBridge } = require("electron");
 
 // this should print out the value of MY_ENVIRONMENT_VARIABLE
-console.log(process.env.SNAP_URL);
+console.log("SNAP_URL = ", process.env.SNAP_URL);
+console.log("DEVICE_ID", process.env.DEVICE_ID);
 
 contextBridge.exposeInMainWorld("env", {
   DEVICE_ID: process.env.DEVICE_ID,
--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -1,13 +1,16 @@
 #! /bin/sh
 
 export SNAP_URL="$(snapctl get url)"
-export DEVICE_ID="$(snapctl get device_id)"
+# export DEVICE_ID="$(snapctl get device_id)"
+export DEVICE_ID="$(hostname)"
+
+echo "DEVICE = $DEVICE_ID"
 
 if cat /proc/cpuinfo | grep -q "Pi 4"; then
   EXTRAOPTS="--disable-gpu"
 fi
 
-exec $SNAP/electron-helloworld/electron-quick-start \
+exec DEVICE_ID="$(hostname)" $SNAP/electron-helloworld/electron-quick-start \
 	--enable-features=UseOzonePlatform \
 	--ozone-platform=wayland \
 	--disable-dev-shm-usage \
