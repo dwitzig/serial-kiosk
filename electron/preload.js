e864801ce2929bec833cf857b8e5510941b7e2a7
Damien <damien@webts.biz>
2023-07-28 15:51:52 +1000

    try access env

--- electron/preload.js
+++ electron/preload.js
@@ -2,9 +2,10 @@
 const { contextBridge } = require("electron");
 
 // this should print out the value of MY_ENVIRONMENT_VARIABLE
+console.log("process.env = ", process.env);
 console.log("SNAP_URL = ", process.env.SNAP_URL);
 console.log("DEVICE_ID=", process.env.DEVICE_ID);
 
-contextBridge.exposeInMainWorld("env", {
-  DEVICE_ID: process.env.DEVICE_ID,
-});
+contextBridge.exposeInMainWorld("process_env", process.env);
+contextBridge.exposeInMainWorld("device_id", process.env.DEVICE_ID);
+contextBridge.exposeInMainWorld("device", "PRM0001");
