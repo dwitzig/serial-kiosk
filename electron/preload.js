c9860dab42eade9ef81b2d3abeb4ff25f01d9926
Damien <damien@webts.biz>
2023-09-08 15:58:28 +1000

    add preload

--- electron/preload.js
+++ electron/preload.js
@@ -3,12 +3,12 @@ const { contextBridge } = require("electron");
 
 // this should print out the value of MY_ENVIRONMENT_VARIABLE
 console.log("process.env = ", process.env);
-console.log("SNAP_URL = ", process.env.SNAP_URL);
+console.log("SNAP_URLx = ", process.env.SNAP_URL);
 console.log("DEVICE_ID=", process.env.DEVICE_ID);
 
 // contextBridge.exposeInMainWorld("process_env", process.env);
 // contextBridge.exposeInMainWorld("device1", process.env.DEVICE_ID);
 // contextBridge.exposeInMainWorld("device1", process.env.DEVICE_ID);
-contextBridge.exposeInMainWorld("device", "man");
-contextBridge.exposeInMainWorld("device1", process.env.DEVICE_ID);
-contextBridge.exposeInMainWorld("device2", DEVICE_ID);
+contextBridge.exposeInMainWorld("DEVICE_MAN", "man");
+contextBridge.exposeInMainWorld("DEVICE_ID", process.env.DEVICE_ID);
+// contextBridge.exposeInMainWorld("device2", DEVICE_ID);
