119bc48040272cf265c0e085b92c569ccd60ed8d
Damien <damien@webts.biz>
2024-05-08 09:12:45 +1000

    try remove --no-sandbox

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -11,8 +11,14 @@ if cat /proc/cpuinfo | grep -q "Pi 4"; then
   EXTRAOPTS="--disable-gpu"
 fi
 
+# exec $SNAP/electron-helloworld/electron-quick-start \
+# 	--enable-features=UseOzonePlatform \
+# 	--ozone-platform=wayland \
+# 	--disable-dev-shm-usage \
+# 	--no-sandbox $EXTRAOPTS
+
 exec $SNAP/electron-helloworld/electron-quick-start \
 	--enable-features=UseOzonePlatform \
 	--ozone-platform=wayland \
 	--disable-dev-shm-usage \
-	--no-sandbox $EXTRAOPTS
+	$EXTRAOPTS
